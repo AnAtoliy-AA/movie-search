170e460d63b551b8cf9574455b873e24eddb53ef
AnAtoliyAK <aliaksandrau89@gmail.com>
2021-01-19 19:51:49 +0300

    fix: change swiper link and remove comments

--- js/header.js
+++ js/header.js
@@ -6,10 +6,8 @@ class Header {
   createHeader() {
     const header = document.createElement('header');
     const navBar = this.createHeaderNavBar();
-    // const searchForm = this.createSearchForm();
 
     header.appendChild(navBar);
-    // header.appendChild(searchForm);
     document.body.appendChild(header);
   }
 
@@ -28,35 +26,6 @@ class Header {
     return navBar;
   }
 
-  // createSearchForm() {
-  //   const searchForm = document.createElement('form');
-  //   const inputContainer = document.createElement('div');
-  //   const searchInput = document.createElement('input');
-  //   const searchLoaderSpiner = document.createElement('div');
-  //   const searchButton = document.createElement('button');
-
-  //   searchForm.id = 'search-form';
-  //   searchForm.classList.add('wrapper', 'form-inline', 'my-2', 'my-lg-0');
-  //   inputContainer.classList.add('input');
-  //   searchInput.id = 'search-text';
-  //   searchInput.classList.add('form-control-lg', 'mr-sm-2');
-  //   searchInput.type = 'search';
-  //   searchInput.autocomplete = 'off';
-  //   searchInput.autofocus = 'on';
-  //   searchInput.placeholder = 'Search movie';
-  //   searchLoaderSpiner.id = 'spiner';
-  //   searchLoaderSpiner.classList.add('loader', 'hidden');
-  //   searchButton.type = 'submit';
-  //   searchButton.classList.add('btn', 'btn-secondary', 'my-2', 'my-sm-0');
-  //   searchButton.innerText = 'Search';
-
-  //   inputContainer.appendChild(searchInput);
-  //   inputContainer.appendChild(searchLoaderSpiner);
-  //   searchForm.appendChild(inputContainer);
-  //   searchForm.appendChild(searchButton);
-
-  //   return searchForm;
-  // }
 }
 
 export default Header;
